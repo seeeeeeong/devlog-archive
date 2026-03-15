8c7b5e11dcdaa54e7192127caf700194d7909e63
seeeeeeong <lsinsung@gmail.com>
2026-03-15 20:58:26 +0900

    fix: 30GB EBS, workflow 파일 자동 동기화 추가

--- infra/terraform/ec2.tf
+++ infra/terraform/ec2.tf
@@ -109,7 +109,7 @@ resource "aws_instance" "main" {
   iam_instance_profile   = aws_iam_instance_profile.ec2.name
 
   root_block_device {
-    volume_size = 20
+    volume_size = 30
     volume_type = "gp3"
     encrypted   = true
   }
